4f8fb3e40336451db4c3b388b00eabfff4914701
SuhailahMohd <165987721+SuhailahMohd@users.noreply.github.com>
2025-09-17 16:44:30 +0800

    save

--- app/Http/Controllers/productController.php
+++ app/Http/Controllers/productController.php
@@ -28,7 +28,6 @@ class ProductController extends Controller
         ]);
 
         Product::create($data);
-
         return redirect()->route('products.index')->with('success', 'Product created.');
     }
 
@@ -46,14 +45,12 @@ class ProductController extends Controller
         ]);
 
         $product->update($data);
-
         return redirect()->route('products.index')->with('success', 'Product updated.');
     }
 
     public function destroy(Product $product)
     {
         $product->delete();
-
         return redirect()->route('products.index')->with('success', 'Product deleted.');
     }
 }
