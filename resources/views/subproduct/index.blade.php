ee31a71b57a9ec104d2c589a828999997c8fbe4c
SuhailahMohd <165987721+SuhailahMohd@users.noreply.github.com>
2025-09-22 11:25:23 +0800

    subproducts index

--- resources/views/subproduct/index.blade.php
+++ resources/views/subproduct/index.blade.php
@@ -7,7 +7,7 @@
   <a href="{{ route('subproducts.create') }}" class="btn btn-primary">Add Subproduct</a>
 </div>
 
-<div class="table-responsive">
+<div class="table-responsive" x-data="subproductList()">
   <table class="table table-bordered align-middle">
     <thead class="table-light">
       <tr>
@@ -19,35 +19,43 @@
       </tr>
     </thead>
     <tbody>
-      @forelse($subproducts as $sp)
-        <tr>
-          <td>{{ $sp->product->name}}</td>
-          <td>{{ $sp->name }}</td>
-          <td class="text-wrap">{{ $sp->description }}</td>
-          <td class="text-end">{{ number_format($sp->price, 2) }}</td>
-          <td>
-            <a href="{{ route('subproducts.edit', $sp) }}" class="btn btn-sm btn-outline-secondary">Edit</a>
-            <form action="{{ route('subproducts.destroy', $sp) }}" method="POST" class="d-inline">
-              @csrf
-              @method('DELETE')
-              <button class="btn btn-sm btn-outline-danger" onclick="return confirm('Delete this subproduct?')">Delete</button>
-            </form>
-          </td>
-        </tr>
-      @empty
+        <template x-if="subproducts.length=== 0">
         <tr>
           <td colspan="5">
             <div class="alert alert-info mb-0">No subproducts yet. Click <strong>Add Subproduct</strong> to create one.</div>
           </td>
         </tr>
-      @endforelse
+        </template>
+
+        <template x- for="subproduct for subproducts" :key="supbproduct.id">
+        <tr>
+          <td x-text="subproduct.product_name"></td>
+          <td x-text="subproduct.name"></td>
+          <td class="text-wrap" x-text="subproduct.description"></td>
+          <td class="text-end"> x-text="formatPrice(subproduct.price)"></td>
+          <td>
+            <a :href="`/subproducts/${sp.id}/edit`" class="btn btn-sm btn-outline-secondary">Edit</a>
+            <form :action="`/subproducts/${sp.id}`" method="POST" class="d-inline">
+              @csrf
+              @method('DELETE')
+              <button class="btn btn-sm btn-outline-danger"
+                      onclick="return confirm('Delete this subproduct?')">
+                Delete
+              </button>
+            </form>
+          </td>
+        </tr>
+      </template>
     </tbody>
   </table>
 </div>
 
-@if(method_exists($subproducts, 'links'))
-  <div class="mt-2">
-    {{ $subproducts->links('pagination::bootstrap-5') }}
-  </div>
-@endif
-@endsection
+@push('js')
+    <script>
+        function subproductList() {
+            return {
+                subproducts: @json($subproducts),
+            }
+        }
+    </script>
+@endpush
